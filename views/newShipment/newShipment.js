0cf587f0f98d3ca76423e61db31700c281692284
marsianin88 <marcin8wiech@gmail.com>
2018-08-16 11:13:58 +0100

    New Shipment js and html

--- views/newShipment/newShipment.js
+++ views/newShipment/newShipment.js
@@ -5,11 +5,21 @@ angular.module('myApp.newShipment', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/newShipment', {
             templateUrl: 'views/newShipment/newShipment.html',
-            controller: 'View1Ctrl'
+            controller: 'newShipmentCtrl'
         });
     }])
 
-    .controller('View1Ctrl', [function() {
+    .controller('newShipmentCtrl', ['$scope', function($scope) {
+
+        $scope.consignments = [];
+
+        $scope.addConsignment = function() {
+            var newConsignmentNumber = $scope.consignments.length+1;
+            $scope.consignments.push({'id': 'Consignment number ' + newConsignmentNumber, 'name': 'Consignment number ' + newConsignmentNumber});
+        };
+
+        $scope.deleteConsignment = function(index) {
+            $scope.consignments.splice(index, 1);
+        };
 
     }]);
-
