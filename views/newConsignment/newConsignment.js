49dbc68b22ccd7eaf6557eafc4038d475bfe7a10
MJFancourt <matt.fancourt@koiosmasterdata.com>
2018-08-16 11:07:08 +0100

    Changed newConsignment HTML and JS

--- views/newConsignment/newConsignment.js
+++ views/newConsignment/newConsignment.js
@@ -5,18 +5,22 @@ angular.module('myApp.newConsignment', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/newConsignment', {
             templateUrl: 'views/newConsignment/newConsignment.html',
-            controller: 'newConsignmentController'
+            controller: 'newConsignmentCtrl'
         });
     }])
 
-    .controller('newConsignmentController', ['$scope', function($scope) {
+    .controller('newConsignmentCtrl', ['$scope', function($scope) {
 
         $scope.products = [];
 
         $scope.addProduct = function() {
-            $scope.products.push({'ISO': $scope.newProduct, 'NoI': $scope.nOI});
+            $scope.products.push({'iso8000Identifier': $scope.newProduct, 'NoI': $scope.nOI});
             $scope.newProduct= '';
             $scope.nOI = '';
+        };
+
+        $scope.deleteProduct = function(index) {
+            $scope.products.splice(index, 1);
         };
 
     }]);
